b93b2e0f75e243b6405840b13336b58324fed2ee
JD Gonzales <jd_gonzales@icloud.com>
2017-05-22 17:49:57 -0500

    updating commit history

--- lib/bloc_works.rb
+++ lib/bloc_works.rb
@@ -1,13 +1,15 @@
 require "bloc_works/version"
 require "bloc_works/dependencies"
 require "bloc_works/controller"
+#require "bloc_works/router"
 require "pry"
 
 module BlocWorks
   class Application
     def call(env)
-      [200, {'Content-Type' => 'text/html'},[]]
+      [200, {'Content-Type' => 'text/html'}, ['text']]
     end
+
   end
 end
 
